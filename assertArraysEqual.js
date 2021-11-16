b6d0fff73642ceacef233b06bf7b9d57db2c4aa8
sujatha-m <jhanavi.bhushan@gmail.com>
2021-11-15 22:29:45 -0500

    Implemented assertArraysEqaul function using eqArrays function

---
+++ assertArraysEqual.js
@@ -0,0 +1,33 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (
+      array1[i] !== array2[i] &&
+      (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))
+    ) {
+      return false;
+    }
+    if (Array.isArray(array1[i] && Array.isArray(array2[i]))) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual(['1','2','3'], ['1','2','3']);
+assertArraysEqual([1,2,3], [1,2,'3']);
+
+
+
+
