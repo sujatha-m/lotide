a1dc2b0d2682ba0cf377b33e8dc221385e23f956
sujatha-m <jhanavi.bhushan@gmail.com>
2021-11-24 13:02:44 -0500

    created test file and removed unwanted duplicate code for eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:   ${actual}===${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed :  ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) return false;
   for (let i = 0; i < array1.length; i++) {
@@ -21,13 +13,5 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 5, 3], [1, 2, 3]));
+module.exports = eqArrays;
 
-console.log(eqArrays(['1', '2', '3'], ['1', '2', '3']));// => true
-console.log(eqArrays(['1', '2', '3'], ['1', '2', 3]));// => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 5, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), false);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
