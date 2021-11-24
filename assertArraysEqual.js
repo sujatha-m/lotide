ec452ef212ada06681d7730aeeaa22b78b3fb2ba
sujatha-m <jhanavi.bhushan@gmail.com>
2021-11-24 13:09:16 -0500

    Modules added and refactored functions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (
-      array1[i] !== array2[i] &&
-      (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))
-    ) {
-      return false;
-    }
-    if (Array.isArray(array1[i] && Array.isArray(array2[i]))) {
-      if (!eqArrays(array1[i], array2[i])) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -24,10 +8,7 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual(['1','2','3'], ['1','2','3']);
-assertArraysEqual([1,2,3], [1,2,'3']);
-
+module.exports = assertArraysEqual;
 
 
 
