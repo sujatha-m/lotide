64ec1bee3132d792e41f267d0bcbb00a6909aa4a
sujatha-m <jhanavi.bhushan@gmail.com>
2021-11-24 13:19:04 -0500

    removed unwanted code in middle.js function

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-  for (let i = 0; i < array1.length; i++) {
-    if (
-      array1[i] !== array2[i] &&
-      (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))
-    ) {
-      return false;
-    }
-    if (Array.isArray(array1[i] && Array.isArray(array2[i]))) {
-      if (!eqArrays(array1[i], array2[i])) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
