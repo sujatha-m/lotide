3377d156d80b242eadf3b8384e9c9b56bab67e6a
sujatha-m <jhanavi.bhushan@gmail.com>
2021-11-26 10:46:44 -0500

    removed test cases from takeUntil file

--- takeUntil.js
+++ takeUntil.js
@@ -13,16 +13,4 @@ const takeUntil = function(array, callback) {
 
 module.exports = takeUntil;
 
-//Test cases
-
-
-//expected output[ 'I\'ve', 'been', 'to', 'Hollywood' ]
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-
-const data3 = [1, 2, 5, 7, 8, 2, -1, 2, 4, 5, 9];
-const results3 = takeUntil(data3, x => x < 0);
-assertArraysEqual(results3, [ 1, 2, 5, 7, 2 ]);
-
 
